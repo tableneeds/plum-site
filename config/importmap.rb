d1727451d9ff0887a922fec3c10186fe688ca6f2
Ben Simmons <bdsimmons1@gmail.com>
2026-08-07 15:34:21 -0500

    Pin Plum 0.2 controllers in external host

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,10 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "controllers", to: "controllers/index.js"
 pin "controllers/application", to: "controllers/application.js"
 pin "controllers/hello_controller", to: "controllers/hello_controller.js"
+
+# Plum 0.2.0's controller directory does not expand in an external host's
+# import map. Pin the packaged files explicitly until the upstream path fix is
+# available in a released gem.
+Plum::Engine.root.join("app/javascript/controllers/plum").glob("*_controller.js").sort.each do |controller|
+  pin "controllers/plum/#{controller.basename(".js")}", to: "plum/#{controller.basename}"
+end
